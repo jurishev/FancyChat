4f0494cac4f6f33c2352854e20c14ff89c2ce68c
Yuri Shevchenko <jurishev@ya.ru>
2020-09-13 19:15:22 +0300

    Support multi connections

--- Chat.Wpf/MainWindowViewModel.cs
+++ Chat.Wpf/MainWindowViewModel.cs
@@ -1,6 +1,9 @@
-﻿using System.Diagnostics;
+﻿using System.Collections.Concurrent;
+using System.Collections.Generic;
+using System.Diagnostics;
 using System.IO;
 using System.Net;
+using System.Net.WebSockets;
 using System.Text;
 
 namespace Chat.Wpf
@@ -8,13 +11,31 @@ namespace Chat.Wpf
     internal class MainWindowViewModel : ObservableModel
     {
         private readonly string html;
+        private readonly HttpListener listener;
 
         private string input;
         private string output;
 
-        private ActionCommand launchCommand;
+        private ActionCommand startListenerCommand;
+        private ActionCommand stopListenerCommand;
+        private ActionCommand launchClientCommand;
+        private ActionCommand sendMessageCommand;
 
-        public MainWindowViewModel() => html = File.ReadAllText("index.html");
+        private readonly ConcurrentQueue<string> outputQueue = new ConcurrentQueue<string>();
+        private readonly ConcurrentBag<WebSocket> aliveSockets = new ConcurrentBag<WebSocket>();
+        private readonly ConcurrentBag<WebSocket> deadSockets = new ConcurrentBag<WebSocket>();
+
+        public MainWindowViewModel()
+        {
+            html = File.ReadAllText("index.html");
+
+            listener = new HttpListener();
+            listener.Prefixes.Add("http://localhost:4000/");
+        }
+
+        public bool ListenerIsListening => listener.IsListening;
+
+        public int AliveSocketCount => aliveSockets.Count;
 
         public string Input
         {
@@ -38,30 +59,58 @@ namespace Chat.Wpf
             }
         }
 
-        public ActionCommand LaunchCommand =>
-            launchCommand ??= new ActionCommand(_ => Listen());
+        public ActionCommand StartListenerCommand =>
+            startListenerCommand ??= new ActionCommand(_ =>
+            {
+                listener.Start();
+                OnPropertyChanged(nameof(ListenerIsListening));
 
-        private async void Listen()
+            }, _ => !ListenerIsListening);
+
+        public ActionCommand StopListenerCommand =>
+            stopListenerCommand ??= new ActionCommand(_ =>
+            {
+                listener.Stop();
+                OnPropertyChanged(nameof(ListenerIsListening));
+
+            }, _ => ListenerIsListening);
+
+        public ActionCommand LaunchClientCommand =>
+            launchClientCommand ??= new ActionCommand(_ => LaunchClient(), _ => ListenerIsListening);
+
+        public ActionCommand SendMessageCommand =>
+            sendMessageCommand ??= new ActionCommand(_ =>
+            {
+                outputQueue.Enqueue(Input);
+                Input = string.Empty;
+
+            }, _ => !string.IsNullOrWhiteSpace(Input));
+
+        private async void LaunchClient()
         {
-            var listener = new HttpListener();
-            listener.Prefixes.Add("http://localhost:4000/");
-            listener.Start();
-
             Process.Start(new ProcessStartInfo()
             {
                 FileName = "http://localhost:4000/",
                 UseShellExecute = true,
             });
 
-            var httpContext = await listener.GetContextAsync();
-            var response = httpContext.Response;
+            var context = await listener.GetContextAsync();
+            var rsp = context.Response;
 
             byte[] buff = Encoding.UTF8.GetBytes(html);
-            response.ContentLength64 = buff.Length;
-            response.OutputStream.Write(buff, 0, buff.Length);
-            response.OutputStream.Close();
+            rsp.ContentLength64 = buff.Length;
+            rsp.OutputStream.Write(buff, 0, buff.Length);
+            rsp.OutputStream.Close();
 
-            listener.Stop();
+            context = await listener.GetContextAsync();
+
+            if (context.Request.IsWebSocketRequest)
+            {
+                var wsc = await context.AcceptWebSocketAsync(null);
+                aliveSockets.Add(wsc.WebSocket);
+
+                OnPropertyChanged(nameof(AliveSocketCount));
+            }
         }
     }
 }
